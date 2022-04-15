adf93b0f4f3afd37f095c8123878887a3ad245d2
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2022-04-15 10:47:25 -0300

    Adicionado crud inicial

---
+++ desafio-dio/desafio-dio/src/main/java/com/projetos/desafiodio/service/ClienteServiceObj.java
@@ -0,0 +1,34 @@
+package com.projetos.desafiodio.service;
+
+import com.projetos.desafiodio.model.Cliente;
+
+import java.util.Iterator;
+
+public class ClienteServiceObj implements ClienteService{
+
+
+    @Override
+    public Iterator<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
